84fa0b9b883a3bcf96182f933038723c2fc32a70
neerajpmahajan19 <neerajpmahajan19@gmail.com>
2026-02-20 17:50:45 +0530

    Initial portfolio upload
    Commit directly to the main branch

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    const triggerBottom = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
